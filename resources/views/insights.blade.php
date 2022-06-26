8f124b1f177f39dacdd51e8c202454f64469d9bb
Kenny Lock <lockkenny2@gmail>
2022-06-26 17:07:39 +0800

    update contant

--- resources/views/insights.blade.php
+++ resources/views/insights.blade.php
@@ -9,7 +9,7 @@
         <h1 class="display-4 font-weight-normal">Copper Trade</h1>
         <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple's marketing pages.</p>
         <a class="btn btn-outline-secondary" href="#">Coming soon</a>
-      </div>
+    </div>
       <div class="product-device box-shadow d-none d-md-block"></div>
       <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
     </div>
