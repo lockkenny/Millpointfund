2fca66f746922f2ed453abbe9f2cbf8017272e2c
Kenny Lock <lockkenny2@gmail>
2022-07-03 11:16:10 +0800

    Update content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,20 @@
   <!-- About section-->
   <section id="about">
   <div class="container">
-    <div class="shadow-lg p-3 mb-2 mt-5 container1">
+    <div class="shadow-lg mb-2 mt-1 container1">
       <div class="intro1">
         <h1 class="shadow-sm p-3 mb-2 mt-5"><span id="AboutTitle"></span></h1>
         <h2 class="p-3 mb-2 mt-1"><span id="AboutTitle1"></span></h2>
      </div>
+     <!-- ANNUAL PERFORMANCE - TOTAL RETURN (%)-->
       <div class="shadow-lg container2">
         <h2 class="shadow-sm p-3 mb-2 mt-5"><span id="Returns"></span></h2> 
-        <div class="p-5 mb-2 mt-1 flourish-embed flourish-chart" data-src="visualisation/6856440"><script src="https://public.flourish.studio/resources/embed.js"></script></div>    
+        <div class="flourish-embed flourish-chart" data-src="visualisation/6856440"><script src="https://public.flourish.studio/resources/embed.js"></script></div>    
       </div>
+      <!-- SECTOR WEIGHTS -->
         <div class="shadow-lg container3">
           <h2 class="shadow-sm p-3 mb-2 mt-5"><span id="Sectors"></span></h2>
-          <div class="p-5 mb-2 mt-1 flourish-embed flourish-chart" data-src="visualisation/6820520"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
+          <div class="flourish-embed flourish-chart" data-src="visualisation/6820520"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
       </div>
   </div>
     </section>
@@ -25,16 +27,17 @@
 <!-- Operations section-->
     <section id="operations">
       <div class="container">
-        <div class="shadow-lg p-3 mb-2 mt-5 container4">
-          <div class="intro2">
+        <!-- MONTHLY - TOTAL RETURNS (%) -->
+        <div class="shadow-lg container4">      
           <h1 class="shadow-sm p-3 mb-2 mt-5"><span id="AboutOpt"></span></h1>
           <h2 class="p-3 mb-2 mt-1"><span id="AboutOpt1"></span></h2>
-        <div class="flourish-embed flourish-chart" data-src="visualisation/6971396"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
-      </div>
+          <div class="flourish-embed flourish-chart" data-src="visualisation/6971396"><script src="https://public.flourish.studio/resources/embed.js"></script></div>  
+        </div>  
+      <!-- TRACK - RECORDS (PRESENT - 2019) -->    
         <div class="shadow-lg container5">
-          <h2 class="shadow-sm p-3 mb-2 mt-5"><span id="Records"></span></h2>
-          <div class="flourish-embed flourish-table" data-src="visualisation/9467778"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
-      </div>
+            <h2 class="shadow-sm p-3 mb-2 mt-5"><span id="Records"></span></h2>
+            <div class="flourish-embed flourish-table" data-src="visualisation/9467778"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
+        </div>
     </div>
     </section>
 
@@ -42,9 +45,7 @@
     <section id="insights">
       <div class="container">
         <div class="shadow-lg p-3 mb-2 mt-5 container6">
-          <div class="intro3">
             <h1 class="shadow-sm p-3 mb-2 mt-5"><span id="AboutInsights"></span></h1>
-          </div>
         </div>
       </div>
     </secton>
