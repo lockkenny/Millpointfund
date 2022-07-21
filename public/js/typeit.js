8441f9d9b9f7274faad8df1648eeb88fc964779a
Kenny Lock <lockkenny2@gmail>
2022-07-21 23:56:49 +0800

    update

--- public/js/typeit.js
+++ public/js/typeit.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
   .go();
 
   new TypeIt("#AboutOpt", {
-    strings: "Operations",
+    strings: "Operations Operations Operations",
     speed: 50,
     startDelay: 900,
     waitUntilVisible: true,
