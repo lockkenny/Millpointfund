164a826389e70e16661d21de8ab295602938de89
Kenny Lock <lockkenny2@gmail>
2022-10-02 23:23:01 +0800

    update

--- api/index.php
+++ api/index.php
@@ -2,11 +2,3 @@
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
-
-if ($_GET['type'] === 'css') {
-    header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
-} else if ($_GET['type'] === 'js') {
-    header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
-}
