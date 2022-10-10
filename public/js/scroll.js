4bd0a8e930c38103e098a91b30ec641788670e19
Kenny Lock <lockkenny2@gmail>
2022-10-11 00:03:49 +0800

    update

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function (event) {
 
     ScrollReveal({ reset: true, useDelay: 'onload' });
-    ScrollReveal().reveal('.container1', { duration:2000, delay:375, origin: 'top', distance:'1%' });
-    ScrollReveal().reveal('.container2', { duration:2000, delay:375, origin: 'bottom', distance:'1%' });
-    ScrollReveal().reveal('.container3', { duration:2000, delay:375, origin: 'top', distance:'1%' });
-    ScrollReveal().reveal('.container4', { duration:2000, delay:375, origin: 'bottom', distance:'1%' });
-    ScrollReveal().reveal('.container5', { duration:2000, delay:375, origin: 'top', distance:'1%' });
-    ScrollReveal().reveal('.container6', { duration:2000, delay:375, origin: 'bottom', distance:'1%' });
+    ScrollReveal().reveal('.container1', { duration:2000, origin: 'top', distance:'100px'});
+    ScrollReveal().reveal('.container2', { duration:2000, origin: 'bottom', distance:'100px' });
+    ScrollReveal().reveal('.container3', { duration:2000, origin: 'top', distance:'100px' });
+    ScrollReveal().reveal('.container4', { duration:2000, origin: 'bottom', distance:'100px' });
+    ScrollReveal().reveal('.container5', { duration:2000, origin: 'top', distance:'100px' });
+    ScrollReveal().reveal('.container6', { duration:2000, origin: 'bottom', distance:'1px', reset: false});
 
 });
