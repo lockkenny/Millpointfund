88e8e4c390756aa47c17c8dbc58a218d91f13005
Kenny Lock <lockkenny2@gmail>
2023-01-29 19:45:26 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Spatie\Sitemap\SitemapGenerator;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
 
@@ -18,4 +18,10 @@ Route::get('/', [WebController::class, 'index']);
 Route::get('/operations', [WebController::class, 'operations']);
 Route::get('/insights', [WebController::class, 'insights']);
 Route::get('/terms', [WebController::class, 'terms']);
+
+Route::get('sitemap', function(){
+
+    SitemapGenerator::create('https://millpointfund.com')->writeToFile('sitemap.xml');
+    return 'sitemap created';
+});
 
