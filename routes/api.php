3bd5ccb32dd079846657e68731b79624bf21bd90
Kenny Lock <lockkenny2@gmail>
2023-04-19 02:15:33 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/sitemap.xml', function (Request $request) {
+    return response()->view('sitemap')->header('Content-Type', 'text/xml');
+});
